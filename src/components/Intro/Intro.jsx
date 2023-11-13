f54c87009c0e2c14a8dbe3ff72ab54df7bb60cd8
barkat21579121 <148468513+barkat21579121@users.noreply.github.com>
2023-11-14 01:19:54 +0500

    read me

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import "./Intro.css";
 import Vector1 from "../../img/Vector1.png";
 import Vector2 from "../../img/Vector2.png";
-// import boy from "../../img/boy.png";
+import boy from "../../img/boy.png";
 import glassesimoji from "../../img/glassesimoji.png";
 import thumbup from "../../img/thumbup.png";
 import crown from "../../img/crown.png";
@@ -48,7 +48,7 @@ const Intro = () => {
       <div className="i-right">
         <img src={Vector1} alt="" />
         <img src={Vector2} alt="" />
-        {/* <img src={boy} alt="" /> */}
+        <img className="img-barkat" src={boy} alt="" />
         {/* animation */}
         <motion.img
           initial={{ left: "-36%" }}
